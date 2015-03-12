f2c2060c3ff27bac339dc231eb117c678f136728
Vanessa Ronan <vanessaronan@Vanessas-MacBook-Pro.local>
2015-03-12 15:12:24 -0700

    Add empty project

---
+++ BikeWithFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BikeWithFriends
+//
+//  Created by Vanessa Ronan on 3/12/15.
+//  Copyright (c) 2015 Jenner Felton, Vanessa Ronan, Sarah Trisorus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BikeWithFriends/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BikeWithFriends
+//
+//  Created by Vanessa Ronan on 3/12/15.
+//  Copyright (c) 2015 Jenner Felton, Vanessa Ronan, Sarah Trisorus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
